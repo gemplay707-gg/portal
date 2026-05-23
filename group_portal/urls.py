5be6b150453bf93a92e8b11e8164642d1cb8a907
gemplay707-gg <gemplay707@gmail.com>
2026-05-23 20:01:03 +0300

    Added login and logout functionality via POST request

--- group_portal/urls.py
+++ group_portal/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portal.urls')), # Підключаємо маршрути нашого порталу
+    path('accounts/', include('django.contrib.auth.urls')),  # Маршрути авторизації
+    path('', include('portal.urls')),  # Маршрути нашого додатка
 ]
